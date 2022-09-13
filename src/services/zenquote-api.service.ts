546edbeac25f1ab8b13e62d1a41e0691153d325d
F7023247 Matheus Phillipe Velozo Amaral <matheus.amaral@bb.com>
2022-09-13 16:58:29 -0300

    Service que pega as quotes da api

---
+++ src/services/zenquote-api.service.ts
@@ -0,0 +1,26 @@
+interface Quote {
+    text: string;
+    author: string;
+}
+
+function extractArray(obj: Array<any>): Array<Quote> {
+    return obj.map((value): Quote => {
+        return { text: value.q, author: value.a };
+    });
+}
+
+function extractUnique(obj: Array<any>): Quote {
+    return extractArray(obj)[0];
+}
+
+export const dailyZQ = async () => {
+    const response = await fetch("https://zenquotes.io/api/today");
+    return extractUnique(await response.json());
+};
+
+export const randomZQ = async (num: number = 50) => {
+    const response = await fetch("https://zenquotes.io/api/quotes");
+    const quotes = extractArray(await response.json());
+
+    return quotes.slice(0, num);
+};
